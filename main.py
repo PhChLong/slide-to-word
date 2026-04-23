59293706009cc6299fbedac6721736ce4bef0f34
PhChLong <phamchaulong10082006@gmail.com>
2026-04-23 21:55:27 +0700

    chores

--- main.py
+++ main.py
@@ -9,14 +9,17 @@ ocr = PaddleOCR(det_model, rec_model)
 
 data_dir = Path("data")
 outputs = []
-image_extensions = {".jpg", ".jpeg", ".png", ".bmp", ".webp"}
-for image_path in data_dir.iterdir():
-    if image_path.is_file() and image_path.suffix.lower() in image_extensions:
-        img = cv2.imread(image_path)
-        #@ yolo detect_crop ảnh
-        #! trống
+# image_extensions = {".jpg", ".jpeg", ".png", ".bmp", ".webp"}
+# for image_path in data_dir.iterdir():
+#     if image_path.is_file() and image_path.suffix.lower() in image_extensions:
+#         img = cv2.imread(image_path)
+#         #@ yolo detect_crop ảnh
+#         #! trống
 
-        #@ trích xuất text
-        out = ocr(img)
-        outputs.append(out) #@ out = dict(index: (bbox, text))
-
+#         #@ trích xuất text
+#         out = ocr(img)
+#         outputs.append(out) #@ out = dict(index: (bbox, text))
+image_path = "image.png"
+img = cv2.imread(image_path)
+out = ocr(img)
+print(out)
